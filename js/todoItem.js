9f062ada590ff7a9be2effeef985f1a3a379e7c1
Prashant Singh <prashant.singh@prolitus.com>
2019-06-25 10:38:55 +0530

    collection and model view render

--- js/todoItem.js
+++ js/todoItem.js
@@ -1,1 +1,7 @@
-var todoItem = Backbone.Model.extend();
+var TodoItem = Backbone.Model.extend({
+    validate: function(attrs) {
+        if(!attrs.description) {
+            return 'Description is required.';
+        }
+    }
+});
